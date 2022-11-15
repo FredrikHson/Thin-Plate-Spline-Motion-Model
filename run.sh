38670cff15eb8961d10c0579da992dadd30771fb
Fredrik Hansson <fredrikhansson_12345@yahoo.com>
2022-11-15 12:48:46 +0100

    added run script using anaconda and changed the requirements

---
+++ run.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+inputfile1=$(readlink -f $1)
+inputfile2=$(readlink -f $2)
+cd $(dirname $(readlink -f $0))
+
+set -e
+source /opt/anaconda/bin/activate root
+condaenv="thin-plate-spline"
+conda env list | grep $condaenv &>/dev/null
+if [[ $? -ne 0 ]]; then
+    needssetup=1
+    conda create --name $condaenv python=3.9
+fi
+conda activate $condaenv
+
+if [[ "$needssetup" -eq 1 ]]; then
+    mkdir -p checkpoints
+    pip3 install torch torchvision torchaudio
+    pip install -r requirements.txt
+fi
+run()
+{
+    python demo.py --config config/vox-256.yaml \
+        --checkpoint checkpoints/vox.pth.tar --source_image $1 \
+        --driving_video $2 --find_best_frame --result_video $1_$(date +%s).mp4
+}
+
+if [[ -z "$inputfile1" ]]; then
+    echo run this script with run.sh image.png video.mp4
+fi
+
+if [[ -z "$inputfile2" ]]; then
+    echo run this script with run.sh image.png video.mp4
+fi
+
+run "$inputfile1" "$inputfile2"
